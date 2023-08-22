11dadc2ae49527113d748dc8ee7d6643222c5189
BaxyD <112361061+BaxyD@users.noreply.github.com>
2023-08-21 19:03:49 -0500

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,15 +5,17 @@ import './App.css'
 
 function App() {
   const [newItem, setNewItem] = useState('')
-  const [todos, setTodos] =useState('')
+  const [todos, setTodos] =useState([])
 function handleSubmit(e) {
   e.preventDefault();
-
-  setTodos (currentTodos => [
+    if (newItem.trim() !== ''){
+      setTodos (currentTodos => [
       ...currentTodos,
       { id: crypto.randomUUID(), title: newItem, completed:
       false },
- ]);
+     ]);
+     setNewItem('');
+    }
 }
 
   return (
@@ -32,10 +34,9 @@ function handleSubmit(e) {
       
       <li key={todo.id}>
         <label>
-          <input type="checkbox" checked={todo.completed}/>
-          {todo.title}
+          <input type="text" id='item' value={newItem} onChange={(e) => setNewItem(e.target.value)}/>
         </label>
-        <button className='Btn btn-danger'>delete</button>
+        <button className='Btn btn-danger' onClick={() => setTodos(currentTodos=> currentTodos.filter(t=> t.id!== todo.id))} >delete</button>
       </li>
       )
     })}
